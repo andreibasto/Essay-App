eefcb09b54eb2cfb0698437d3539b9e72ef514aa
ARedFish <109739015+ARedFish@users.noreply.github.com>
2022-07-29 08:46:01 -0400

    Update main.dart

--- lib/lib/main.dart
+++ lib/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MaterialApp(
-        navigatorKey: navigatorKey,
+        //navigatorKey: navigatorKey,
         debugShowCheckedModeBanner: false,
         home: MainPage(),
   );
